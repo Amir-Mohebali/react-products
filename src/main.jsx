f495407bedd9b93a6d6eab6ee3deacb4b962d766
Amir Mohebali <amir.mohebali76@gmail.com>
2024-09-03 20:26:09 +0330

    add fonts and base style

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
+import './assets/fonts/Poppins/Poppins-Regular.ttf'
+import './assets/fonts/Lobster/Lobster-Regular.ttf'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
