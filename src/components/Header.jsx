ad78a8b1d4752ba1c929fe0746d298468e93eaca
Amir Mohebali <amir.mohebali76@gmail.com>
2024-09-03 23:12:45 +0330

    feat: add header to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,43 @@
+import { NavLink } from 'react-router-dom'
+import { FaSearch, FaShoppingCart } from 'react-icons/fa'
 import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='nav'>
+        <NavLink to='/'>
+            <h3 className='logo'>
+                <span>Smart</span> Shop
+            </h3>
+        </NavLink>
+        <ul className="nav-links">
+            <li>
+                <NavLink to='/'>Home</NavLink>
+                <div className="underline"></div>
+            </li>
+            <li>
+                <NavLink to='/products/phones'>Phones</NavLink>
+                <div className="underline"></div>
+            </li>
+            <li>
+                <NavLink to='/products/laptops'>Laptops</NavLink>
+                <div className="underline"></div>
+            </li>
+            <li>
+                <NavLink to='/products/tvs'>TVs</NavLink>
+                <div className="underline"></div>
+            </li>
+        </ul>
+        <div className="nav-controls">
+            <div className="search-btn">
+                <FaSearch />
+            </div>
+            <div className="cart-btn">
+                <FaShoppingCart/>
+                <div className="qty">0</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
